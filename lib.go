5c6fa46c5b58d237ab0544e6019814c2f2b12e61
Carlos Cobo <toqueteos@gmail.com>
2017-10-04 17:51:39 +0200

    v2.0.1: Add package documentation and to all public members

--- lib.go
+++ lib.go
@@ -1,3 +1,4 @@
+// Package pausereader provides pausable features into any io.Reader.
 package pausereader
 
 import (
@@ -6,6 +7,8 @@ import (
 	"sync/atomic"
 )
 
+// PausableReader defines a representation of pausable `io.Reader`s.
+// It is implemented by PauseReader.
 type PausableReader interface {
 	io.Reader
 	Pause()
@@ -28,22 +31,28 @@ func New(r io.Reader) *PauseReader {
 		state: 1,
 	}
 }
+
+// Pause pauses all future calls to `Read` making them blocking.
 func (r *PauseReader) Pause() {
 	if atomic.CompareAndSwapUint32(&r.state, 1, 0) {
 		r.wg.Add(1)
 	}
 }
 
+// Resume resumes all pending calls to `Read`, if the underlying `io.Reader` is
+// not goroutine-safe then expect bad things to happen.
 func (r *PauseReader) Resume() {
 	if atomic.CompareAndSwapUint32(&r.state, 0, 1) {
 		r.wg.Done()
 	}
 }
 
+// IsPaused returns true if this PauseReader is paused or false otherwise.
 func (r *PauseReader) IsPaused() bool {
 	return atomic.LoadUint32(&r.state) == 0
 }
 
+// Read implements io.Reader it'll block if Pause is called until Resume is called.
 func (r *PauseReader) Read(buf []byte) (n int, err error) {
 	if r.IsPaused() {
 		r.wg.Wait()
